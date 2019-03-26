a787ba0f82c00cf2eeed9d8a69f84c565876f087
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2019-03-26 17:35:41 +0100

    php basics
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

---
+++ basics/20190326_beispiele.php
@@ -0,0 +1,84 @@
+<?php 
+
+/**
+ * Dieses Skript läuft möglicherweise nicht durch oder produziert Fehler,
+ * es geht nur um die Beispiele
+ */
+
+/**
+ * Arrays
+ */
+var arr = [1, 2, 3];
+
+$arr = [
+    'vorname' => 'Regina',
+    "sae-kurs" => 'WDD317',
+    'drei' => 3,
+    4 => 4
+];
+
+/**
+ * Datentyp prüfen
+ */
+is_bool($arr);
+is_string('string');
+
+/**
+ * Operatoren
+ */
+$modulo = 33 % 2;
+
+/**
+ * Inkrement/Dekrement
+ */
+$a = 42;
+$b = ++$a; // 43
+$c = $a++; // 43
+echo $a; // 44
+
+$a = 42;
+$b = --$a; // 41
+// ....
+
+/**
+ * Referenzen & Funktionen
+ */
+function addString (&$str) {
+    return "$str - append";
+}
+
+$a = "string";
+addString($a);
+echo $a; // string - append
+
+// oder
+ 
+function addString ($str) {
+    return "$str - append";
+}
+
+$a = "string";
+$a = addString($a);
+echo $a; // string - append
+
+/**
+ * Referenz Beispiel
+ */
+$arr1 = [1, 2, 3];
+$wert = array_shift($arr1); // 1
+var_dump($arr1); // [2, 3]
+
+// While Loop
+while (true) {
+    // läuft theoretisch unendlich lang
+}
+
+while (false) {
+    // läuft gar nicht
+}
+
+do {
+    echo "foobar"; // gibt 1x "foobar" aus
+} while (false);
+
+?>
