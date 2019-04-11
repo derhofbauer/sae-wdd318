ed10aecf92d98258e66a0bfa3d9136991f4aebc1
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2019-04-11 16:45:17 +0200

    user list

--- mysql/index.php
+++ mysql/index.php
@@ -3,3 +3,26 @@
 require_once 'dbconnect.php';
 
 ?>
+
+
+<div class="user-list">
+    <header>Users</header>
+    <ul class="users">
+        <?php
+            $sql = "SELECT id,name FROM users;";
+
+            $result = mysqli_query($link, $sql);
+            
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<li class=\"id-{$row['id']}\">{$row['name']}</li>";
+            }
+        ?>
+    </ul>
+</div>
+
+
+<?php
+
+require_once 'dbclose.php';
+
+?>
