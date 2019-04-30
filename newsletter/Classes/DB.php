8b75c0dfcda28fdc0bd18906ef7260d7f6bdef02
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2019-04-30 14:17:54 +0200

    cleanup

--- newsletter/Classes/DB.php
+++ newsletter/Classes/DB.php
@@ -12,58 +12,49 @@ class DB {
         $this->link = new mysqli($host, $user, $pass, $dbname, $port);
     }
 
-/**
- * query
- * @param  string $sql
- * @param  array  $params ['i:id' => 2, 's:name' => "Thomas"]
- */
-public function query($sql, array $params = []) {
+    /**
+     * query
+     * @param  string $sql
+     * @param  array  $params ['i:id' => 2, 's:name' => "Thomas"]
+     */
+    public function query($sql, array $params = []) {
         // query abschicken
         // $this->stmt = $this->link->stmt_init();
-    $this->stmt = $this->link->prepare($sql);
+        $this->stmt = $this->link->prepare($sql);
 
         // prepare params if there is at least one
-    if (count($params)) {
-        $types = '';
-        $param_values = [];
-        foreach ($params as $key => $value) {
-            
-            $values = explode(':', $key); // 'i:id' => [0 => 'i', 1 => 'id']
-            $type = $values[0]; // i, bzw. s
-            $name = $values[1]; // id, bzw. name
+        if (count($params)) {
+            $types = '';
+            $param_values = [];
+            foreach ($params as $key => $value) {
 
-            $types .= $type;
-            $param_values[] = &$value; // mysqli_stmt::bind_param erwartet referenzen auf die Werte und nicht die werte selbst: https://www.php.net/manual/de/mysqli-stmt.bind-param.php
+                $values = explode(':', $key); // 'i:id' => [0 => 'i', 1 => 'id']
+                $type = $values[0]; // i, bzw. s
+                $name = $values[1]; // id, bzw. name
 
-        } // types: 'is', values: [2, 'Thomas']
+                $types .= $type;
+                $param_values[] = &$value; // mysqli_stmt::bind_param erwartet referenzen auf die Werte und nicht die werte selbst: https://www.php.net/manual/de/mysqli-stmt.bind-param.php
 
-        // bind params
-        array_unshift($param_values, $types); // params: ['is', 2, 'Thomas']
-        call_user_func_array([$this->stmt, 'bind_param'], $param_values); // $this->stmt->bind_param($params[0], $params[1], $params[2], .....);
-    }
+            } // types: 'is', values: [2, 'Thomas']
+
+            // bind params
+            array_unshift($param_values, $types); // params: ['is', 2, 'Thomas']
+            call_user_func_array([$this->stmt, 'bind_param'], $param_values); // $this->stmt->bind_param($params[0], $params[1], $params[2], .....);
+        }
 
         // execute query
-    $this->stmt->execute();
+        $this->stmt->execute();
 
         // get result
-    $result = $this->stmt->get_result();
+        $result = $this->stmt->get_result();
 
-    return $result->fetch_all(MYSQLI_ASSOC);
-}
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 
-public function __destruct () {
+    public function __destruct () {
         // Datenbankverbindung schließen
-    $this->link->close();
+        $this->link->close();
+    }
 }
-}
-
-// $host = 'mariadb'; // bei MAMP/XAMPP/etc.: localhost
-// $user = 'newsletter';
-// $pass = 'password';
-// $dbname = 'newsletter';
-// $db = new DB($host, $user, $pass, $dbname);
-
-// $users = $db->query('SELECT * FROM users WHERE id = ?', ['i:id' => 1]);
-// var_dump($users);
 
 ?>
